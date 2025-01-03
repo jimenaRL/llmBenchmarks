7283bc8e1c0044578d823f791ac06966c9814b25
jimenaRL <jimena@melix.net>
2025-01-03 16:44:15 +0000

    Add port argument

--- asyncBenchmark.py
+++ asyncBenchmark.py
@@ -8,30 +8,40 @@ from argparse import ArgumentParser
 ap = ArgumentParser()
 ap.add_argument('--framework', type=str, choices=["ollama", "vllm"], default="ollama")
 ap.add_argument('--model', type=str)
-ap.add_argument('--promtsFile', type=str, default="")
+ap.add_argument('--port', type=str, default="")
+ap.add_argument('--promptsFile', type=str, default="")
 ap.add_argument('--prePrompt', type=str, default="")
 args = ap.parse_args()
 framework = args.framework
 model = args.model
+port = args.port
 prePrompt = args.prePrompt
-promtsFile = args.promtsFile
+promptsFile = args.promptsFile
 
 modelname = model.split('/')[-1]
 
 if framework == 'ollama':
-    URL = "http://localhost:11434/api/generate"
+    if port:
+        PORT = port
+    else:
+        PORT = 11434
+    URL = f"http://localhost:{PORT}/api/generate"
     HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
     DATA = '{"model": "${model}", "prompt": "${prompt}", "stream": false, "keep_alive": "25m"}'
 else: # framework == 'vllm'
-    URL = "http://localhost:8000/v1/completions"
+    if port:
+        PORT = port
+    else:
+        PORT = 8000
+    URL = f"http://localhost:{PORT}/v1/completions"
     HEADERS = {'Content-Type': 'application/json'}
-    DATA = '{"model": "${model}", "prompt": "${prompt}", "max_tokens": 7, "temperature": 0}'
+    DATA = '{"model": "${model}", "prompt": "${prompt}", "max_tokens": 200, "temperature": 0}'
 
-TESTPROMPT = "How much is 1+3?"
-if not promtsFile:
+TESTPROMPT = "1+3?"
+if not promptsFile:
     prompts = [TESTPROMPT]
 else:
-    with open(promtsFile) as csvfile:
+    with open(promptsFile) as csvfile:
         reader = csv.DictReader(csvfile)
         prompts = [row['prompt'] for row in reader]
 
@@ -47,11 +57,11 @@ async def do_post(session, url, model, prompt):
     async with session.post(url, data=Template(DATA).substitute(model=model, prompt=prompt), headers=HEADERS) as response:
         data = await response.text()
         print(f">>>>> Requesting prompt {prompt}")
-        hashvalue = hashlib.sha1(prompt.encode()).hexdigest()[:6]
-        filename = f'{framework}_{modelname}_prompt_{hashvalue}.txt'
+        hashvalue = hashlib.sha1(prompt.encode()).hexdigest()[:8]
+        filename = f'{framework}_{modelname}_{hashvalue}.txt'
         with open(filename, 'w') as file:
             file.write(data)
- 
+
 async def run_all():
     async with aiohttp.ClientSession() as session:
         post_tasks = []
