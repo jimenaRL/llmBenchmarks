b4439f198ab3ca0ae0fecb8dc39c56bca48a132d
jimenaRL <jimena@melix.net>
2025-01-14 01:58:06 +0100

    Added max token arg

--- multimodalBenchmark.py
+++ multimodalBenchmark.py
@@ -13,12 +13,14 @@ from argparse import ArgumentParser
 ap = ArgumentParser()
 ap.add_argument('--images_urls_file', type=str, required=False, default="")
 ap.add_argument('--port', type=str, required=False, default='8000')
+ap.add_argument('--max_tokens', type=str, required=False, default='64')
 ap.add_argument('--text', type=str, required=False, default='What’s in this image?')
 ap.add_argument('-v', '--verbose',  action='store_true')
 args = ap.parse_args()
 images_urls_file = args.images_urls_file
 port = args.port
 text = args.text
+max_tokens = args.max_tokens
 verbose = args.verbose
 
 TESTIMAGEURL = "https://www.oceano.org/wp-content/uploads/2019/06/06_M%C3%A9duse-Pelagia-noctiluca_M_Dagnino-scaled.jpg"
@@ -73,7 +75,7 @@ for image_url in images_urls:
             ],
         }],
         model=model,
-        max_tokens=64,
+        max_tokens=max_tokens,
     )
 
     result = chat_completion_from_base64.choices[0].message.content
