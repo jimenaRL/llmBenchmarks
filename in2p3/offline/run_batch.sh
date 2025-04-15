a44832bcf613e8763befc8cfb89e563b8ef06bbd
jimenaRL <jimena@melix.net>
2025-04-15 17:23:49 +0200

    Add referefrence

--- in2p3/offline/run_batch.sh
+++ in2p3/offline/run_batch.sh
@@ -1,3 +1,6 @@
+# Check https://docs.vllm.ai/en/stable/getting_started/examples/openai.html
+# and https://github.com/vllm-project/vllm/blob/main/vllm/entrypoints/openai/run_batch.py
+
 python /sps/humanum/user/jroyolet/dev/vllm/vllm/entrypoints/openai/run_batch.py \
     -i test_for_llava.jsonl \
     -o llava_results.jsonl \
