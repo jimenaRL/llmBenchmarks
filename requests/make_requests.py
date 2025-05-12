4e9c7d0eab98f6c96213c83c19aae8ff5e139cf4
jimenaRL <jimena@melix.net>
2025-05-12 14:17:13 +0200

    Add args to make pattern script

--- requests/make_requests.py
+++ requests/make_requests.py
@@ -2,10 +2,16 @@ import re
 import csv
 import json
 from string import Template
+from argparse import ArgumentParser
 
-requestTemplate = "requestPattern.json"
-biosPath = "biosExample.csv"
-outputJsonl = "requestBatch.jsonl"
+ap = ArgumentParser()
+ap.add_argument('--request_template', type=str, required=False, default="patternRequest.json")
+ap.add_argument('--bios_path', type=str, required=False, default="chargeLoadTest.csv")
+ap.add_argument('--output_file', type=str, required=False, default="batchRequest.jsonl")
+args = ap.parse_args()
+requestTemplate = args.request_template
+biosPath = args.bios_path
+outputJsonl = args.output_file
 
 # Validate and load the input request pattern,
 # this is equivalent to make pattern = f.read()
@@ -21,13 +27,16 @@ with open(biosPath, 'r') as csvfile:
 
 # Create a batch of request in the form of a jsonl file
 # by substituting each bio into the request pattern
-requests =  [Template(pattern).substitute(id=i, userbio=b) for i, b in enumerate(bios)]
+requests =  [Template(pattern).substitute(id=i, userbio=json.dumps(b)) for i, b in enumerate(bios)]
 dumped_requests = map(json.dumps, requests)
 with open(outputJsonl, "w") as f:
-    f.writelines('\n'.join(dumped_requests))
+    # f.writelines('\n'.join(dumped_requests))
+    f.writelines('\n'.join(requests))
 
-# Validate the created jsonl file
-with open(outputJsonl, "r") as f:
-    reloaded_requests = list(map(json.loads, f.readlines()))
+# # Validate the created jsonl file
+# with open(outputJsonl, "r") as f:
+#    reloaded_requests = list(map(json.loads, f.readlines()))
 
-assert len(bios) == len(reloaded_requests)
+assert len(bios) == len(requests)
+
+print(f"Jsonl output file wrote to {outputJsonl} with {len(requests)} lines.")
