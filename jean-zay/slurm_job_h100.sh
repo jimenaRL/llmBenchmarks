f35ddd227972ffbe7592950cf4dc2161affa9fe8
jimenaRL <jimena@melix.net>
2025-05-12 15:57:30 +0200

    Add h100 slurm job

--- jean-zay/slurm_job_h100.sh
+++ jean-zay/slurm_job_h100.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=vllmTest_%j.log   # name of output file
 #SBATCH --error=vllmTest_%j.error  # name of error file (here, common with the output file)
 
-#SBATCH -C a100                # Choix de partition (here gpu_p5 partition 80GB A100 GPU)
+#SBATCH -C h100                # Choix de partition (here gpu_p5 partition 80GB A100 GPU)
 #SBATCH --gres=gpu:1                 # number of GPUs per node (max 8 with gpu_p2, gpu_p5)
 #SBATCH --nodes=1                    # number of nodes
 #SBATCH --ntasks-per-node=3          # number of MPI tasks per node (= number of GPUs per node)
@@ -13,21 +13,17 @@
 
 #SBATCH --time=20:00:00             # Déli max
 
-#SBATCH --account=nmf@a100              # Set acccount
+#SBATCH --account=nmf@h100              # Set acccount
 
 #SBATCH --mail-user=jimena.royoletelier@sciencespo.fr # Où envoyer l'e-mail
 #SBATCH --mail-type=ALL          # Événements déclencheurs (NONE, BEGIN, END, FAIL, ALL)
 
-# Example script for running and query a vllm server on a slurm job.
-# Before running the job you must launch un interactive terminal a install vllm on a python virtual enviroment
-# as explained on the points 1-4 here:
-# https://github.com/jimenaRL/llmBenchmarks/blob/main/in2p3/openai_chat_completion_client_for_multimodal.py
-
 # Cleans out modules loaded in interactive and inherited by default
 module purge
 
-# load python enviroment
+# Load python enviroment
 module load python/3.12.7
+conda init
 conda activate vllm
 
 # Get args
