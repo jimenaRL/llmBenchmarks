87ffb475ca883bb134ea591d4ae78b5ffe656302
jimenaRL <jimena@melix.net>
2025-05-14 11:13:50 +0200

    Update slurm scripts

--- jean-zay/slurm_job_a100.sh
+++ jean-zay/slurm_job_a100.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=vllmTest # Nom du jobe
-#SBATCH --output=vllmTest_%j.log   # name of output file
-#SBATCH --error=vllmTest_%j.error  # name of error file (here, common with the output file)
+#SBATCH --output=vllmTest_a100_%j.log   # name of output file
+#SBATCH --error=vllmTest_a100_%j.error  # name of error file (here, common with the output file)
 
 #SBATCH -C a100                # Choix de partition (here gpu_p5 partition 80GB A100 GPU)
 #SBATCH --gres=gpu:1                 # number of GPUs per node (max 8 with gpu_p2, gpu_p5)
@@ -18,11 +18,6 @@
 #SBATCH --mail-user=jimena.royoletelier@sciencespo.fr # Où envoyer l'e-mail
 #SBATCH --mail-type=ALL          # Événements déclencheurs (NONE, BEGIN, END, FAIL, ALL)
 
-# Example script for running and query a vllm server on a slurm job.
-# Before running the job you must launch un interactive terminal a install vllm on a python virtual enviroment
-# as explained on the points 1-4 here:
-# https://github.com/jimenaRL/llmBenchmarks/blob/main/in2p3/openai_chat_completion_client_for_multimodal.py
-
 # Cleans out modules loaded in interactive and inherited by default
 module purge
 
@@ -30,15 +25,16 @@ module purge
 module load python/3.12.7
 conda activate vllm
 
-# Change directory
-cd /linkhome/rech/gensoj01/umu89ib/.conda/envs/vllm/lib/python3.12/site-packages/vllm/entrypoints/openai
-
 # Get args
 INPUT=$1
 OUTPUT=$2
-MODEL=$1
+MODEL=$3
+
+# Set env variables
+export HF_HUB_OFFLINE=1
+export HUGGINGFACE_HUB_CACHE=/lustre/fswork/projects/rech/nmf/umu89ib/.cache/huggingface
 
 # Run command
-cmd="python run_batch.py -i ${INPUT} -o ${OUTPUT} --model ${MODEL} --gpu-memory-utilization=0.9 --max_model_len=21500 --dtype=half"
+cmd="python /linkhome/rech/gensoj01/umu89ib/.conda/envs/vllm/lib/python3.12/site-packages/vllm/entrypoints/openai/run_batch.py -i ${INPUT} -o ${OUTPUT} --model=${MODEL} --gpu-memory-utilization=0.9 --max_model_len=21500 --dtype=half"
 echo "[RUNNING] ${cmd}"
 eval "$cmd"
