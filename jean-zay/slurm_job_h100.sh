74587970fdba403acf7da8c86cd562daadbeeb61
jimenaRL <jimena@melix.net>
2025-05-15 08:49:41 +0200

    Update slurm jobs

--- jean-zay/slurm_job_h100.sh
+++ jean-zay/slurm_job_h100.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=vllmTest # Nom du jobe
-#SBATCH --output=vllmTest_%j.log   # name of output file
-#SBATCH --error=vllmTest_%j.error  # name of error file (here, common with the output file)
+#SBATCH --job-name=vllmZephyrH100 # Nom du jobe
+#SBATCH --output=logs/%j.log   # name of output file
+#SBATCH --error=errors/%j.error  # name of error file (here, common with the output file)
 
 #SBATCH -C h100                # Choix de partition (here gpu_p5 partition 80GB A100 GPU)
 #SBATCH --gres=gpu:1                 # number of GPUs per node (max 8 with gpu_p2, gpu_p5)
@@ -26,15 +26,18 @@ module load python/3.12.7
 conda init
 conda activate vllm
 
-# Change directory
-cd /linkhome/rech/gensoj01/umu89ib/.conda/envs/vllm/lib/python3.12/site-packages/vllm/entrypoints/openai
-
 # Get args
 INPUT=$1
 OUTPUT=$2
-MODEL=$1
+
+# Set env variables
+export HF_HUB_OFFLINE=1
+export HUGGINGFACE_HUB_CACHE=/lustre/fswork/projects/rech/nmf/umu89ib/.cache/huggingface
+
+# Show nvidia setting for logging
+nvidia-smi
 
 # Run command
-cmd="python run_batch.py -i ${INPUT} -o ${OUTPUT} --model ${MODEL} --gpu-memory-utilization=0.9 --max_model_len=21500 --dtype=half"
+cmd="python /linkhome/rech/gensoj01/umu89ib/.conda/envs/vllm/lib/python3.12/site-packages/vllm/entrypoints/openai/run_batch.py -i ${INPUT} -o ${OUTPUT} --model=HuggingFaceH4/zephyr-7b-beta --gpu-memory-utilization=0.9 --max_model_len=21500 --dtype=half"
 echo "[RUNNING] ${cmd}"
 eval "$cmd"
