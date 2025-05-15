7300dd661fa078df3fe1f49d285e52fc77a2fcd0
jimenaRL <jimena@melix.net>
2025-05-15 09:14:16 +0200

    Update slurm jobs in2p3

---
+++ in2p3/slurm_job_h100_zephyr.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# SLURM options:
+
+#SBATCH --job-name=vllmZephyrH100 # Nom du job
+#SBATCH --output=logs/%j.log   # Standard output et error log
+
+#SBATCH --partition=gpu               # Choix de partition
+#SBATCH --gres=gpu:h100:1
+
+#SBATCH --ntasks=1                    # Exécuter une seule tâche
+#SBATCH --mem=16G                    # Mémoire en MB par défaut
+#SBATCH --time=6-23:59             # Déli max
+
+#SBATCH --mail-user=jimena.royoletelier@sciencespo.fr # Où envoyer l'e-mail
+#SBATCH --mail-type=ALL          # Événements déclencheurs (NONE, BEGIN, END, FAIL, ALL)
+
+source /sps/humanum/user/jroyolet/environments/vllm0.8.4/bin/activate
+
+# Get args
+INPUT=$1
+OUTPUT=$2
+
+# Run command
+cmd="python /sps/humanum/user/jroyolet/environments/vllm0.8.4/lib64/python3.9/site-packages/vllm/entrypoints/openai/run_batch.py \
+    -i ${INPUT} \
+    -o ${OUTPUT} \
+    --model=HuggingFaceH4/zephyr-7b-beta \
+    --gpu-memory-utilization=0.9 \
+    --max_model_len=21500 \
+    --dtype=half"
+
+echo "[RUNNING] ${cmd}"
+eval "$cmd"
