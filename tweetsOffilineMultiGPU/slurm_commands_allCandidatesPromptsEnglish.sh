511fa235cfa0d1eaf717b1ca9449e8b76eec38f3
jimenaRL <jimena@melix.net>
2025-09-02 18:54:38 +0200

    Add slurm commands for free/guided and binary/multiple llm quering strategies

--- tweetsOffilineMultiGPU/slurm_commands_allCandidatesPromptsEnglish.sh
+++ tweetsOffilineMultiGPU/slurm_commands_allCandidatesPromptsEnglish.sh
@@ -197,7 +197,7 @@ sbatch \
 
 -------------------------------------------------------------------------------
 
-export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"max_model_len\": 8000, \"tensor_parallel_size\": 1}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.7, \"min_p\": 0, \"top_p\": 0.8, \"top_k\": 20, \"seed\": 19, \"max_tokens\": 256}'"
 export CHOICES="'YES,NO'"
 export OUTFOLDER=1xH100_max_model_len_8000_Qwen3-30B-A3B-Instruct-2507/guided
@@ -208,7 +208,7 @@ sbatch \
     --export=ALL \
     binaryChoicesPromptsEnglish.slurm
 
-export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"max_model_len\": 8000, \"tensor_parallel_size\": 1}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.7, \"min_p\": 0, \"top_p\": 0.8, \"top_k\": 20, \"seed\": 19, \"max_tokens\": 256}'"
 export CHOICES="'Macron,Mélenchon,Le Pen,None'"
 export OUTFOLDER=1xH100_max_model_len_8000_Qwen3-30B-A3B-Instruct-2507/guided
@@ -220,7 +220,7 @@ sbatch \
     multipleChoicesPromptsEnglish.slurm
 
 
-export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"max_model_len\": 8000, \"tensor_parallel_size\": 1}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.7, \"min_p\": 0, \"top_p\": 0.8, \"top_k\": 20, \"seed\": 19, \"max_tokens\": 256}'"
 export CHOICES="''"
 export OUTFOLDER=1xH100_max_model_len_8000_Qwen3-30B-A3B-Instruct-2507/free
@@ -232,7 +232,7 @@ sbatch \
     binaryChoicesPromptsEnglish.slurm
 
 
-export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export MODELPARAMS="'{\"model\": \"Qwen/Qwen3-30B-A3B-Instruct-2507\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"max_model_len\": 8000, \"tensor_parallel_size\": 1}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.7, \"min_p\": 0, \"top_p\": 0.8, \"top_k\": 20, \"seed\": 19, \"max_tokens\": 256}'"
 export CHOICES="''"
 export OUTFOLDER=1xH100_max_model_len_8000_Qwen3-30B-A3B-Instruct-2507/free
@@ -245,3 +245,147 @@ sbatch \
 
 -------------------------------------------------------------------------------
 
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-24B-Instruct-2501\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'YES,NO'"
+export OUTFOLDER=1xH100_Mistral-Small-24B-Instruct-2501/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-24B-Instruct-2501\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'Macron,Mélenchon,Le Pen,None'"
+export OUTFOLDER=1xH100_Mistral-Small-24B-Instruct-2501/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-24B-Instruct-2501\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=1xH100_Mistral-Small-24B-Instruct-2501/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-24B-Instruct-2501\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=1xH100_Mistral-Small-24B-Instruct-2501/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
+
+
+--------------------------------------------------------------------------------
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-3.1-24B-Instruct-2503\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'YES,NO'"
+export OUTFOLDER=1xH100_Mistral-Small-3.1-24B-Instruct-2503/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-3.1-24B-Instruct-2503\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'Macron,Mélenchon,Le Pen,None'"
+export OUTFOLDER=1xH100_Mistral-Small-3.1-24B-Instruct-2503/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-3.1-24B-Instruct-2503\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=1xH100_Mistral-Small-3.1-24B-Instruct-2503/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Small-3.1-24B-Instruct-2503\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 1}'"
+export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=1xH100_Mistral-Small-3.1-24B-Instruct-2503/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:1 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
+
+
+-------------------------------------------------------------------------------
+
+export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 7000, \"seed\": 119, \"tensor_parallel_size\": 2}'"
+export SAMPLINGPARAMS="'{\"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'YES,NO'"
+export OUTFOLDER=2xH100_max_model_len_7000_Llama-3.3-70B-Instruct/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:2 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 7000, \"seed\": 119, \"tensor_parallel_size\": 2}'"
+export SAMPLINGPARAMS="'{\"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="'Macron,Mélenchon,Le Pen,None'"
+export OUTFOLDER=2xH100_max_model_len_7000_Llama-3.3-70B-Instruct/guided
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:2 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 7000, \"seed\": 119, \"tensor_parallel_size\": 2}'"
+export SAMPLINGPARAMS="'{\"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=2xH100_max_model_len_7000_Llama-3.3-70B-Instruct/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:2 \
+    --export=ALL \
+    binaryChoicesPromptsEnglish.slurm
+
+
+export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 7000, \"seed\": 119, \"tensor_parallel_size\": 2}'"
+export SAMPLINGPARAMS="'{\"seed\": 19, \"max_tokens\": 256}'"
+export CHOICES="''"
+export OUTFOLDER=2xH100_max_model_len_7000_Llama-3.3-70B-Instruct/free
+sbatch \
+    --job-name=${OUTFOLDER} \
+    --output=$(pwd)/${OUTFOLDER}/%j.log  \
+    --gres=gpu:h100:2 \
+    --export=ALL \
+    multipleChoicesPromptsEnglish.slurm
