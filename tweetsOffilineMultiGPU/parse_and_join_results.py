d12655b6da3db9f985c803cc4bbeb5f671d4a83f
jimenaRL <jimena@melix.net>
2025-09-07 10:46:20 +0200

    Add results parsing

--- tweetsOffilineMultiGPU/parse_and_join_results.py
+++ tweetsOffilineMultiGPU/parse_and_join_results.py
@@ -5,7 +5,7 @@ import  pandas as pd
 # BASEPATH = "/sps/humanum/user/jroyolet/dev/llmBenchmarks/tweetsOffilineMultiGPU"
 BASEPATH = "/home/jimena/work/dev/llmBenchmarks/tweetsOffilineMultiGPU"
 
-models = [
+MODELS = [
     "zephyr-7b-beta",
     "gpt-oss-20b",
     "Mistral-Small-3.1-24B-Instruct-2503",
@@ -18,27 +18,92 @@ models = [
     "Mistral-Large-Instruct-2411"
 ]
 
+ANNOTATIONS = {
 
-def extract_data(model, annotation, df=None, columns=[]):
+    "multiple": [
+        "voteintention/multiple/all",
+        "support/multiple/all",
+        # "criticism/multiple/all"
+    ],
 
-    folders = glob(os.path.join(BASEPATH, f"*{model}*"))
+    "binary" : [
+        "criticism/binary/lepen",
+        "criticism/binary/macron",
+        "criticism/binary/melenchon",
+        "support/binary/lepen",
+        "support/binary/macron",
+        "support/binary/melenchon",
+        "voteintention/binary/lepen",
+        "voteintention/binary/macron",
+        "voteintention/binary/melenchon",
+    ]
+}
+
+CHOICES = {
+    "multiple": ["Macron", "Mélenchon", "Le Pen", "None"],
+    "binary" : ["YES", "NO"]
+}
+
+SETTINGS = ANNOTATIONS.keys()
+
+def parseAnwers(whole_answer, setting):
+
+    whole_answer = whole_answer \
+        .strip() \
+        .split('\n')[0] \
+        .replace("(", "") \
+        .replace(")", "") \
+        .replace(":", "") \
+        .replace("'", "") \
+        .replace('"', "") \
+        .replace(".", "") \
+        .replace("·", "") \
+        .replace(",", "")
+
+    if setting == "binary":
+        whole_answer = whole_answer.upper()
+
+
+    annotation = whole_answer
+    for choice in CHOICES[setting]:
+        if whole_answer[:len(choice)] == choice:
+            annotation = choice
+
+    return annotation
+
+def extract_data(model, annotation, setting, df=None, columns=[]):
+
+    assert model in MODELS
+    assert setting in SETTINGS
+
+    folders = glob(os.path.join(BASEPATH, "outputs/v1", f"*{model}*"))
     assert len(folders) == 1
-    folder = folders [0]
+    folder = folders[0]
+    # print(f"Reading results for model {model} at {folder}")
 
+    # get free answers
+    colname = "FREE-" + model
     path_free = os.path.join(folder, "free", annotation, "llm_answer_0.csv")
-    df_free = pd.read_csv(path_free)
-    df_free = df_free.rename(columns={"answer": "FREE-" + model})
+    df_free = pd.read_csv(path_free) \
+        .fillna("None") \
+        .rename(columns={"answer": colname})
 
     if df is not None:
         df = df.merge(df_free, on=['idx', 'tweet'])
     else:
         df = df_free
 
+    # get guided answers
+    colname = "GUIDED-" + model
     path_guided = os.path.join(folder, "guided", annotation, "llm_answer_0.csv")
-    df_guided = pd.read_csv(path_guided)
+    df_guided = pd.read_csv(path_guided) \
+        .fillna("None") \
+        .rename(columns={"answer": colname})
+    df_guided[colname] = df_guided[colname].apply(lambda a: parseAnwers(a, setting))
+
     if "gpt-oss" in model:
-        df_guided["answer"] = df_guided["answer"].apply(lambda s: s.split("assistantfinal")[-1])
-    df_guided = df_guided.rename(columns={"answer": "GUIDED-" + model})
+        df_guided[colname] = df_guided[colname] \
+            .apply(lambda s: s.split("assistantfinal")[-1])
 
     df = df.merge(df_guided, on=['idx', 'tweet'])
 
@@ -46,40 +111,22 @@ def extract_data(model, annotation, df=None, columns=[]):
 
     return df, columns
 
-annotations = [
-    "voteintention/multiple/all",
-    "support/multiple/all",
-    "criticism/multiple/all"
-]
+for setting in ANNOTATIONS:
 
-annotations = [
-    "criticism/binary/lepen",
-    "criticism/binary/macron",
-    "criticism/binary/melenchon",
-    "support/binary/lepen",
-    "support/binary/macron",
-    "support/binary/melenchon",
-    "voteintention/binary/lepen",
-    "voteintention/binary/macron",
-    "voteintention/binary/melenchon",
-]
+    for annotation in ANNOTATIONS[setting]:
 
-for annotation in annotations:
+        df = None
+        columns = ["idx", "tweet"]
 
-    df = None
-    columns = ["idx", "tweet"]
+        for model in MODELS:
+            try:
+                df, columns = extract_data(model, annotation, setting, df, columns)
+            except Exception as exc:
+                print(exc)
+                print("Continuing...")
 
-    for model in models:
-        try:
-            df, columns = extract_data(model, annotation, df, columns)
-        except Exception as exc:
-            print(exc)
-            print("Continuing...")
+        path = f"{annotation.replace('/', '_')}.csv"
 
-    df = df.fillna("None")
-
-    path = f"{annotation.replace('/', '_')}.csv"
-
-    df[columns].to_csv(path, index=False)
-    print(f"Results for annotation {annotation} results saved at {path}.")
-
+        df[columns].to_csv(path, index=False)
+        print(f"Results for annotation {annotation} results saved at {path}")
+        os.system(f"xan shuffle {path} | xan slice -l 5 | xan v")
