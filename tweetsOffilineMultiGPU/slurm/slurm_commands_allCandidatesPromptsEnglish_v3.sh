edc9c2d7ea0e4eed1f8b9f01f5e6e8ea90284ef7
jimenaRL <jimena@melix.net>
2025-10-18 18:11:09 +0200

    Fix options

--- tweetsOffilineMultiGPU/slurm/slurm_commands_allCandidatesPromptsEnglish_v3.sh
+++ tweetsOffilineMultiGPU/slurm/slurm_commands_allCandidatesPromptsEnglish_v3.sh
@@ -1,6 +1,6 @@
 export MODELPARAMS="'{\"model\": \"meta-llama/Llama-3.3-70B-Instruct\", \"guided_decoding_backend\": \"xgrammar\", \"max_model_len\": 7000, \"seed\": 119, \"tensor_parallel_size\": 2}'"
 export SAMPLINGPARAMS="'{\"seed\": 19, \"max_tokens\": 256}'"
-export CHOICES="'Macron,Mélenchon,Le Pen,Aucun'"
+export CHOICES="'Macron,Mélenchon,Le Pen,None'"
 export OUTFOLDER=outputs/v3/2xH100_max_model_len_7000_Llama-3.3-70B-Instruct/guided
 sbatch \
     --job-name=${OUTFOLDER} \
@@ -12,7 +12,7 @@ sbatch \
 
 export MODELPARAMS="'{\"model\": \"mistralai/Mistral-Large-Instruct-2411\", \"tokenizer_mode\": \"mistral\", \"config_format\": \"mistral\", \"load_format\": \"mistral\", \"guided_decoding_backend\": \"xgrammar\", \"seed\": 119, \"tensor_parallel_size\": 4}'"
 export SAMPLINGPARAMS="'{\"temperature\": 0.15, \"seed\": 19, \"max_tokens\": 256}'"
-export CHOICES="'Macron,Mélenchon,Le Pen,Aucun'"
+export CHOICES="'Macron,Mélenchon,Le Pen,None'"
 export OUTFOLDER=outputs/v3/4xH100_Mistral-Large-Instruct-2411/guided
 sbatch \
     --job-name=${OUTFOLDER} \
